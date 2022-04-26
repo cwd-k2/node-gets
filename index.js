de15ebbe2524cca7eca1c60883ca0624b8711635
cwd-k2 <29105991+cwd-k2@users.noreply.github.com>
2022-04-26 13:02:09 +0900

    Added internal buffer map object You can now use one shared buffer among the instances for the same file descriptor. At the same time, when you want to delete the internal buffer for a fd, now `removeGetsBuffer` function is available.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /**
+ * A map that links `fd` with a buffer
+ * @type {Map<number, Buffer>} BUFFER
+ */
+const BUFFER = new Map();
+
+/**
  * Creates a `gets` function instance with specific configuration.
  *
- * NOTICE: One `gets` instance has one internal buffer each.
- * You should avoid creating multiple `gets` instance for a same file descriptor.
+ * NOTICE: You can create multiple `gets` instance for a file descriptor.
+ * The internal buffer will be shared among the instances.
  *
  * @param fd {number} The file descriptor. Defaults to `0` (STDIN).
  * @param bufsize {number} The size of the internal buffer. Defaults to `32768`.
@@ -11,18 +17,17 @@
  *
  * @return {() => string | undefined} The `gets` function instance. At the end of the stream, it returns `undefined`.
  */
-function createGets(
-  fd = 0,
-  bufsize = 32768,
-  chunksize = 2048,
-  encoding = "utf8"
-) {
+function createGets(fd = 0, bufsize = 32768, chunksize = 2048, encoding = "utf8") {
   /** Be valid. */
   if (bufsize <= 0) bufsize = 32768;
   if (chunksize <= 0) chunksize = 2048;
 
-  /** where the read bytes are stored. */
-  let BUFFER = Buffer.allocUnsafe(_sub2exp(bufsize));
+  /** create or grow a buffer for `fd`; where the read bytes are stored. */
+  if (!BUFFER.has(fd)) {
+    BUFFER.set(fd, Buffer.allocUnsafe(_sub2exp(bufsize)));
+  } else if (bufsize > BUFFER.get(fd).length) {
+    _grow(bufsize);
+  }
   /** start index of returned string in buffer. */
   let ISTART = 0;
   /** offset index of the buffer. indicates where to append the read bytes. */
@@ -35,69 +40,79 @@ function createGets(
   }
 
   /**
-   * Read bytes into BUFFER
+   * Read bytes into the buffer
    *
    * @return {number} The last valid bytes in BUFFER.
    */
   function _read() {
-    // shift BUFFER if needed
-    if (OFFSET + chunksize > BUFFER.length && ISTART !== 0) {
-      BUFFER.copyWithin(0, ISTART, OFFSET);
+    let buffer = BUFFER.get(fd);
+    // shift the buffer if needed
+    if (OFFSET + chunksize > buffer.length && ISTART !== 0) {
+      buffer.copyWithin(0, ISTART, OFFSET);
       OFFSET = OFFSET - ISTART;
       ISTART = 0;
     }
-    // grow BUFFER if needed
-    if (OFFSET + chunksize > BUFFER.length) {
-      const newbuf = Buffer.allocUnsafe(
-        _sub2exp(OFFSET + chunksize, BUFFER.length)
-      );
-      BUFFER.copy(newbuf, 0, 0, OFFSET);
-      BUFFER = newbuf;
+    // grow the buffer if needed
+    if (OFFSET + chunksize > buffer.length) {
+      _grow(OFFSET + chunksize);
+      buffer = BUFFER.get(fd);
     }
     // read bytes synchronously
-    const bytes = require("fs").readSync(fd, BUFFER, OFFSET, chunksize);
+    const bytes = require("fs").readSync(fd, buffer, OFFSET, chunksize);
     return OFFSET + bytes;
   }
 
+  /** Grow the internal buffer. */
+  function _grow(atleast) {
+    const buffer = BUFFER.get(fd);
+    const newbuf = Buffer.allocUnsafe(_sub2exp(atleast, buffer.length));
+    buffer.copy(newbuf, 0, 0, OFFSET);
+    BUFFER.set(fd, newbuf);
+  }
+
   /**
-   * Search a `item` in BUFFER from `searchstart` until `searchend`.
+   * Search a `item` in the buffer, from `searchstart` until `searchend`.
    *
    * @return {number} The index of the first found delimiter. If not found, returns `-1`.
    */
   function _find(item, searchstart, searchend) {
-    for (let i = searchstart; i < searchend; i++)
-      if (BUFFER[i] === item) return i;
+    const buffer = BUFFER.get(fd);
+    for (let i = searchstart; i < searchend; i++) if (buffer[i] === item) return i;
     return -1;
   }
 
   /**
-   * Create string from BUFFER
+   * Create string from the buffer.
    *
    * @param cutidx {number}
-   *   where the delimiter ('\n') is in BUFFER
+   *   where the delimiter ('\n') is in the buffer.
    * @param bufend {number}
-   *   The last valid byte in BUFFER.
+   *   The last valid byte in the buffer.
    */
   function _string(cutidx, bufend) {
-    // unread bytes should be stored in BUFFER
-    const line = BUFFER.toString(encoding, ISTART, cutidx + 1);
+    // unread bytes should be stored in the buffer
+    const line = BUFFER.get(fd).toString(encoding, ISTART, cutidx + 1);
     ISTART = cutidx + 1;
     OFFSET = bufend;
     return line;
   }
 
   /**
-   * Create string from BUFFER
+   * Create string from the buffer.
    */
   function _string0() {
     if (ISTART === OFFSET) return;
-    const line = BUFFER.toString(encoding, ISTART, OFFSET);
+    const line = BUFFER.get(fd).toString(encoding, ISTART, OFFSET);
     ISTART = 0;
     OFFSET = 0;
     return line;
   }
 
   return function () {
+    if (!BUFFER.has(fd)) {
+      throw new Error(`No buffer for fd ${fd}, maybe a disabled gets instance?`);
+    }
+
     if (ISTART !== OFFSET) {
       // if OFFSET is not ISTART (unread bytes left)
       const cutidx = _find(0x0a, ISTART, OFFSET);
@@ -114,6 +129,16 @@ function createGets(
   };
 }
 
+/**
+ * Remove a buffer internally allocated for the file descriptor `fd`.
+ * After calling this function, you can no longer use the `gets` instances for `fd`, until you create new ones.
+ * @param fd {number} The file descriptor.
+ */
+function removeGetsBuffer(fd) {
+  BUFFER.delete(fd);
+}
+
 module.exports = {
   createGets,
+  removeGetsBuffer,
 };
